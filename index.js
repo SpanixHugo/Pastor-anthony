404b19d7fdd62cc1b09ff68652bc3ecc1043e727
Hugo <129804402+SpanixHugo@users.noreply.github.com>
2025-06-01 11:14:17 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -30,3 +30,20 @@ elements.forEach(item => {
     item.style.opacity = '0';
     observer.observe(item);
 });
+
+// const scriptURL = 'https://script.google.com/macros/s/AKfycby1-xD6fB1nApVa99lzs86qbVVI0SvuVRCs1biG-x6p-YX5G7FLoN_od1tvHGcNVUFL/exec'
+// const form = document.forms['submit-to-google-sheet']
+// const msg = document.getElementById("msg")
+
+// form.addEventListener('submit', e => {
+//     e.preventDefault()
+//     fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+//     .then(response => {
+//         msg.innerHTML = "Message sent successfully"
+//         setTimeout(function () {
+//             msg.innerHTML = ""
+//         }, 5000)
+//         form.reset()
+//     })
+//     .catch(error => console.error('Error!', error.message))
+// })
